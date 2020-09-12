9501a77fa4fb7a436fb1463f54d500876c2295a0
Atreya Bain <christophervrose@gmail.com>
2020-09-12 12:44:23 +0530

    Reformat

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {
-  Paper,
-  TextField,
-  Container,
-  Typography,
-  InputAdornment,
-  IconButton,
-  Button,
-  Snackbar,
-  Backdrop,
-  CircularProgress,
+    Paper,
+    TextField,
+    Container,
+    Typography,
+    InputAdornment,
+    IconButton,
+    Button,
+    Snackbar,
+    Backdrop,
+    CircularProgress,
 } from "@material-ui/core";
 import Alert from '@material-ui/lab/Alert';
 import { makeStyles } from "@material-ui/core/styles";
@@ -18,186 +18,185 @@ import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(4)
-  },
-  paper: {
-    padding: theme.spacing(8)
-  },
-  button: {
-    color: "#00629B",
-  },
-  backdrop: {
-    zIndex: 100,
-    color: '#fff',
-  },
+    root: {
+        padding: theme.spacing(4)
+    },
+    paper: {
+        padding: theme.spacing(8)
+    },
+    button: {
+        color: "#00629B",
+    },
+    backdrop: {
+        zIndex: 100,
+        color: '#fff',
+    },
 }));
 
 export default function Signin() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    ieeeid: "",
-    password: "",
-    ieeeidValid: true,
-    passwordValid: true,
-    showPassword: false,
-    authFail: false,
-    networkError: false,
-    incorrectInfo: false,
-  });
-
-  const [backdrop, setBackdrop] = React.useState(false)
-
-  function validateValues(prop, value) {
-    console.log(prop)
-    if (prop === "ieeeid") {
-      const re = /^\d{10}$/;
-      return re.test(String(value).toLowerCase());
-    } else {
-      const re = /^.{8,}$/;
-      return re.test(String(value).toLowerCase());
-    }
-  }
-
-  // Handle changes on text and updates value
-  const handleChange = (prop) => (event) => {
-    setValues({
-      ...values,
-      [prop]: event.target.value,
-      [prop + "Valid"]: validateValues(prop, event.target.value),
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        ieeeid: "",
+        password: "",
+        ieeeidValid: true,
+        passwordValid: true,
+        showPassword: false,
+        authFail: false,
+        networkError: false,
+        incorrectInfo: false,
     });
-  };
 
-  /**
-   *
-   * @param {React.MouseEvent<HTMLInputElement, MouseEvent>} event
-   */
-  const onSubmitSignIn = async (event) => {
-    setBackdrop(true)
-    if(values.ieeeidValid && values.passwordValid){
-      await axios.post("http://forseti-full.herokuapp.com/api/auth", {
-        uid: values.ieeeid,
-        pwd: values.password
-      })
-      .then(res => {
-        if(res.data.ok === true && res.data.auth === true)
-        {
-          localStorage.setItem('atoken', res.data.atoken)
-          localStorage.setItem('isAuthenticated', true)
-          window.location.replace(window.location.origin)
+    const [backdrop, setBackdrop] = React.useState(false)
+
+    function validateValues(prop, value) {
+        console.log(prop)
+        if (prop === "ieeeid") {
+            const re = /^\d{10}$/;
+            return re.test(String(value).toLowerCase());
+        } else {
+            const re = /^.{8,}$/;
+            return re.test(String(value).toLowerCase());
+        }
+    }
+
+    // Handle changes on text and updates value
+    const handleChange = (prop) => (event) => {
+        setValues({
+            ...values,
+            [prop]: event.target.value,
+            [prop + "Valid"]: validateValues(prop, event.target.value),
+        });
+    };
+
+    /**
+     *
+     * @param {React.MouseEvent<HTMLInputElement, MouseEvent>} event
+     */
+    const onSubmitSignIn = async (event) => {
+        setBackdrop(true)
+        if (values.ieeeidValid && values.passwordValid) {
+            try {
+                const res = await axios.post("http://forseti-full.herokuapp.com/api/auth", {
+                    uid: values.ieeeid,
+                    pwd: values.password
+                })
+
+                if (res.data.ok === true && res.data.auth === true) {
+                    localStorage.setItem('atoken', res.data.atoken)
+                    localStorage.setItem('isAuthenticated', true)
+                    window.location.replace(window.location.origin)
+                }
+                else {
+                    setValues({ ...values, ieeeidValid: false, passwordValid: false, authFail: true })
+                    setBackdrop(false)
+                }
+            } catch (err) {
+                console.error(`Axios request failed: ${err}`)
+                if (err.response.status === 401) {
+                    setValues({ ...values, ieeeidValid: false, passwordValid: false, authFail: true })
+                    setBackdrop(false)
+                }
+                else {
+                    setValues({ ...values, ieeeidValid: false, passwordValid: false, networkError: true })
+                    setBackdrop(false)
+                }
+            }
         }
         else {
-          setValues({...values, ieeeidValid: false, passwordValid: false, authFail: true})
-          setBackdrop(false)
+            setValues({
+                ...values,
+                incorrectInfo: !values.ieeeidValid && !values.passwordValid,
+            })
+            setBackdrop(false)
         }
-      })
-      .catch(err => {
-        console.error(`Axios request failed: ${err}`)
-        if(err.response.status === 401){
-          setValues({...values, ieeeidValid: false, passwordValid: false, authFail: true})
-          setBackdrop(false)
-        }
-        else {
-          setValues({...values, ieeeidValid: false, passwordValid: false, networkError: true})
-          setBackdrop(false)
-        }
-      })
+    };
+
+    // Handling show and hide password
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+    // So that the usual mouse down activity doesn't happen
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    // Handle closing of snackbars
+    const handleClose = (prop) => (event, reason) => {
+        if (reason === 'clickaway')
+            return;
+        setValues({ ...values, [prop]: false })
     }
-    else{
-      setValues({
-        ...values,
-        incorrectInfo: !values.ieeeidValid && !values.passwordValid,
-      })
-      setBackdrop(false)
-    }
-  };
 
-  // Handling show and hide password
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
-
-  // So that the usual mouse down activity doesn't happen
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  // Handle closing of snackbars
-  const handleClose = (prop) => (event, reason) => {
-    if(reason === 'clickaway')
-      return;
-    setValues({...values, [prop]:false})
-  }
-
-  return (
-    <Container maxWidth="sm" className={classes.root}>
-      <Paper className={classes.paper}>
-        <Typography variant="h4">Sign in</Typography>
-        <br />
-        <div>
-          <TextField
-            id="ieeeid"
-            label="IEEE ID"
-            type="number"
-            placeholder="Enter your IEEE ID"
-            variant="outlined"
-            fullWidth
-            error={!values.ieeeidValid}
-            onChange={handleChange("ieeeid")}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-        </div>
-        <br />
-        <div>
-          <TextField
-            id="standard-adornment-password"
-            label="Password"
-            placeholder="Enter your password"
-            error={!values.passwordValid}
-            InputLabelProps={{
-              shrink: true
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-            fullWidth
-            type={values.showPassword ? "text" : "password"}
-            value={values.password}
-            variant="outlined"
-            onChange={handleChange("password")}
-          />
-        </div>
-        <br />
-        <Snackbar open={values.incorrectInfo} autoHideDuration={6000} onClose={handleClose('incorrectInfo')}>
-          <Alert elevation={6} variant="filled" onClose={handleClose('incorrectInfo')} severity="error">Incorrect Information entered</Alert>
-        </Snackbar>
-        <Snackbar open={values.authFail} autoHideDuration={6000} onClose={handleClose('authFail')}>
-          <Alert elevation={6} variant="filled" onClose={handleClose('authFail')} severity="error">Invalid username or password</Alert>
-        </Snackbar>
-        <Snackbar open={values.networkError} autoHideDuration={6000} onClose={handleClose('networkError')}>
-          <Alert elevation={6} variant="filled" onClose={handleClose('networkError')} severity="error">Failed connecting to server</Alert>
-        </Snackbar>
-        <Backdrop className={classes.backdrop} open={backdrop}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-        <div>
-          <Button variant="outlined" color="inherit" className={classes.button} onClick={onSubmitSignIn}>
-            Submit
+    return (
+        <Container maxWidth="sm" className={classes.root}>
+            <Paper className={classes.paper}>
+                <Typography variant="h4">Sign in</Typography>
+                <br />
+                <div>
+                    <TextField
+                        id="ieeeid"
+                        label="IEEE ID"
+                        type="number"
+                        placeholder="Enter your IEEE ID"
+                        variant="outlined"
+                        fullWidth
+                        error={!values.ieeeidValid}
+                        onChange={handleChange("ieeeid")}
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                    />
+                </div>
+                <br />
+                <div>
+                    <TextField
+                        id="standard-adornment-password"
+                        label="Password"
+                        placeholder="Enter your password"
+                        error={!values.passwordValid}
+                        InputLabelProps={{
+                            shrink: true
+                        }}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                        fullWidth
+                        type={values.showPassword ? "text" : "password"}
+                        value={values.password}
+                        variant="outlined"
+                        onChange={handleChange("password")}
+                    />
+                </div>
+                <br />
+                <Snackbar open={values.incorrectInfo} autoHideDuration={6000} onClose={handleClose('incorrectInfo')}>
+                    <Alert elevation={6} variant="filled" onClose={handleClose('incorrectInfo')} severity="error">Incorrect Information entered</Alert>
+                </Snackbar>
+                <Snackbar open={values.authFail} autoHideDuration={6000} onClose={handleClose('authFail')}>
+                    <Alert elevation={6} variant="filled" onClose={handleClose('authFail')} severity="error">Invalid username or password</Alert>
+                </Snackbar>
+                <Snackbar open={values.networkError} autoHideDuration={6000} onClose={handleClose('networkError')}>
+                    <Alert elevation={6} variant="filled" onClose={handleClose('networkError')} severity="error">Failed connecting to server</Alert>
+                </Snackbar>
+                <Backdrop className={classes.backdrop} open={backdrop}>
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+                <div>
+                    <Button variant="outlined" color="inherit" className={classes.button} onClick={onSubmitSignIn}>
+                        Submit
           </Button>
-        </div>
-      </Paper>
-    </Container>
-  );
+                </div>
+            </Paper>
+        </Container>
+    );
 }
